a48f0639b5dab4b5f450e61873c7e30de6af1855
marco guardigli <mgua@tomware.it>
2025-01-05 12:47:27 +0100

    activated which-key plugin by folke

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -4,10 +4,10 @@
 
 return require('lazy').setup({
   -- UI Enhancements
-  {
-    'nvim-tree/nvim-web-devicons',
-    lazy = false,
-  },
+  { 'nvim-tree/nvim-web-devicons', lazy = false,  },
+    --  { 'folke/lazy.nvim', version = false },
+    --  { 'LazyVim/LazyVim', version = false },
+
   
   -- File Explorer
   {
@@ -195,6 +195,23 @@ return require('lazy').setup({
       vim.g.mkdp_page_title = '「${name}」'
     end,
   },
+
+  {
+	-- WhichKey is a useful plugin for Neovim 0.5 that displays a popup with possible key bindings 
+	-- of the command you started typing.
+    "folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+	  vim.o.timeout = true
+	  vim.o.timeoutlen = 300
+	end,
+	opts = {
+    	-- your configuration comes here
+    	-- or leave it empty to use the default settings
+    	-- refer to the configuration section below
+	}
+  },
+
 
   -- Theme
   {
