15b329afaab55ea2ba42df8e310773d67b1c088a
Klaudija Stefan <kstefan@tomware.it>
2025-12-30 19:25:07 +0100

    changed treesitter loading in lazy.lua: now apparently works for python too

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -122,36 +122,44 @@ return require('lazy').setup({
     --    dependencies = { 'nvim-treesitter/nvim-treesitter' },
     -- },
     config = function()
-        require('nvim-treesitter.install').compilers = { "gcc" }  -- if using gcc
-        require('nvim-treesitter.configs').setup({  -- was 'nvim-treesitter'
-            -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
-            -- install_dir = vim.fn.stdpath('data') .. '/site',
-            -- require('nvim-treesitter').install({ 'rust', 'javascript', 'zig' }):wait(300000) -- wait max. 5 minutes
-            ensure_installed = { "python", "lua", "vim", "vimdoc", "bash", "powershell", "markdown", "json", "yaml",  },
-            sync_install = false,  -- Install parsers synchronously (only applied to `ensure_installed`)
-            auto_install = true,   -- Automatically install missing parsers when entering buffer
-            -- ignore_install = {},   -- List of parsers to ignore installing (for "all")
-            highlight = {
-                enable = true,
-                --  disable = function(lang, buf)
-                --     local max_filesize = 100 * 1024 -- 100 KB
-                --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                --     if ok and stats and stats.size > max_filesize then
-                --         return true
-                --     end
-                --  end,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true, },
-            -- incremental_selection = {
-            -- enable = true,
-            -- keymaps = {
-            --        init_selection = '<C-space>', node_incremental = '<C-space>',
-            --        scope_incremental = false, node_decremental = '<bs>',
-            --    },
-            -- },
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "python", "vim", "vimdoc", "bash", "powershell", "markdown", "json", "yaml" },
+            callback = function(args)
+                pcall(vim.treesitter.start, args.buf)
+            end,
         })
     end,
+ --   config = function()
+    --    require('nvim-treesitter.install').compilers = { "gcc" }  -- if using gcc
+    --    require('nvim-treesitter.configs').setup({  -- was 'nvim-treesitter' instead of 'nvim-treesitter.configs'
+    --        -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+    --        -- install_dir = vim.fn.stdpath('data') .. '/site',
+    --        -- require('nvim-treesitter').install({ 'rust', 'javascript', 'zig' }):wait(300000) -- wait max. 5 minutes
+    --        ensure_installed = { "python", "lua", "vim", "vimdoc", "bash", "powershell", "markdown", "json", "yaml",  },
+    --        sync_install = false,  -- Install parsers synchronously (only applied to `ensure_installed`)
+    --        auto_install = true,   -- Automatically install missing parsers when entering buffer
+    --        -- ignore_install = {},   -- List of parsers to ignore installing (for "all")
+    --        highlight = {
+    --            enable = true,
+    --            --  disable = function(lang, buf)
+    --            --     local max_filesize = 100 * 1024 -- 100 KB
+    --            --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    --            --     if ok and stats and stats.size > max_filesize then
+    --            --         return true
+    --            --     end
+    --            --  end,
+    --            additional_vim_regex_highlighting = false,
+    --        },
+    --        indent = { enable = true, },
+    --        -- incremental_selection = {
+    --        -- enable = true,
+    --        -- keymaps = {
+    --        --        init_selection = '<C-space>', node_incremental = '<C-space>',
+    --        --        scope_incremental = false, node_decremental = '<bs>',
+    --        --    },
+    --        -- },
+    --    })
+ --   end,
   },
 
 
