ec684f0c04081748ed42847ea98303afa6b1371d
mgua <mgua@tomware.it>
2026-01-19 13:14:49 +0100

    edited ts.lua removing c, lua, vim, vimdoc parsers which are not included in core

--- lua/config/ts.lua
+++ lua/config/ts.lua
@@ -17,13 +17,15 @@
 --
 vim.notify('ts.lua additional setup...', vim.log.levels.INFO)
 
-myparsers = { "html","css","c","lua","python","vim","vimdoc","powershell","bash","markdown","markdown_inline","json","yaml","awk","perl"}
+-- myparsers = { "html","css","c","lua","python","vim","vimdoc","powershell","bash","markdown","markdown_inline","json","yaml","awk","perl"}
+myparsers = { "html","css","python","powershell","bash","markdown","markdown_inline","json","yaml","awk","perl"}
+-- removed c lua vim vimdoc because these are included in core nvim 0.11
 vim.api.nvim_create_autocmd('FileType', {
     pattern = myparsers,
     callback = function()
-        vim.treesitter.start()                                              -- syntax highlighting, provided by Neovim
-        -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'                 -- folds, provided by Neovim
-        -- vim.wo.foldmethod = 'expr'
+        vim.treesitter.start()                                              -- syntax highlighting by ts
+        vim.wo.foldmethod = 'expr'                                          -- fold, ts based with expr and not 'syntax' (not ts based)
+        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'                 -- 
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"   -- indentation, provided by nvim-treesitter (in visual mode?)
     end,
 })
